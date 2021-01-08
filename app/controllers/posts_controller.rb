1f3c5451e3f1891b53a23c91517ad0e51b7c2d12
Arvin Fernandez <arvinf07@gmail.com>
2021-01-08 16:20:27 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-
+ 
   # GET /posts/1
   # GET /posts/1.json
   def show
@@ -63,11 +63,11 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      @post = Post.find(params[:id]) 
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :content, :tag_ids => [])
+      params.require(:post).permit(:name, :content, :tag_ids => [], tags_attributes: [:name])
     end
 end
